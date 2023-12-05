72df6b210981360a17f4d7c495320396c3372a8c
koldnast <sweaterweather00@mail.ru>
2023-12-05 19:01:15 +0400

    Добавление интерфейса для ученика

---
+++ StudentInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SchoolSystem
+{
+    public class StudentInterface: GeneralInterface
+    {
+        public Student Student;
+
+        public void AddAnswerHomework()
+        {
+            var answer = Console.ReadLine();
+        }
+    }
+}
+
