73007ce2100f881100742cd9ae5a33a10fc9a07b
levnast <levnast04@gmail.com>
2023-12-05 18:04:30 +0400

    Добавление класса Авторизация

---
+++ AuthorizationClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem
+{
+    public class AuthorizationClass : User
+    {
+       public void Registration(Dictionary <string,string> DataBase, string login,  string password)
+        {
+            if (!DataBase.ContainsKey(login))
+            {
+                DataBase.Add(login, password);
+            }
+        }
+        public bool Confirmation(Dictionary<string, string> DataBase, string login, string password) 
+        {
+            if (DataBase[login] == password)
+            {
+                return true;
+            }
+            else 
+                return false;
+        }
+    }
+}
