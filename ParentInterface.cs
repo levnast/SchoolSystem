581eca9ce7da3be132db5d9daa4cce101a6094b2
koldnast <sweaterweather00@mail.ru>
2023-12-05 19:00:02 +0400

    Добавление интерфейса для родителя

---
+++ ParentInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SchoolSystem
+{
+    public class ParentInterface: GeneralInterface
+    {
+        public Parent Parent;
+    }
+}
