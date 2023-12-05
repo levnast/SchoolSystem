d9b6c009aeb9cecab3f55967e2cb354efa69fa8a
levnast <levnast04@gmail.com>
2023-12-05 18:09:50 +0400

    Добавление формы домашнего задания

---
+++ Homework.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem
+{
+    public class Homework
+    {
+        public string Object;
+        public string Answer;
+        public int Grade;
+        public DateTime DateOfStart;
+        public DateTime DateOfFinish;
+        public DateTime DateOfCheck;
+    }
+}
