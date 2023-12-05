5810929a936bae83fa8e9e8edadf410922f13a3e
levnast <levnast04@gmail.com>
2023-12-05 18:08:29 +0400

    Добавление формы оценок

---
+++ Grades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem
+{
+    public class Grades
+    {
+        public string Student;
+        public int Grade;
+        public double AverageGrade;
+        public string Object;
+    }
+}
