3094e94b061008d9a2d3ac4a55e96846e45272c1
levnast <levnast04@gmail.com>
2023-12-05 18:07:24 +0400

    Добавление формы расписания

---
+++ Attendance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem
+{
+    public class Attendance
+    {
+        public ScheduleOfLessons Schedule;
+        public bool Presence;
+    }
+}
