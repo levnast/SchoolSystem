0bbc69256e0aa6804ebd649a453cff69cc24a126
koldnast <sweaterweather00@mail.ru>
2023-12-05 18:59:17 +0400

    Добавления общего интерфейса

---
+++ GeneralInterface.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SchoolSystem
+{
+    public class GeneralInterface
+    {
+        public ScheduleOfLessons Schedule;
+        public Grades Assessments;
+        public Attendance Attendance;
+        public Homework Homework;
+
+        public void ViewScheduleOfLessons()
+        {
+            string[] lines = File.ReadAllLines("ScheduleOfLessons.txt");
+            foreach (string s in lines)
+                Console.WriteLine(s);
+
+        }
+        public void ViewGrades()
+        {
+            string[] lines = File.ReadAllLines("Grades.txt");
+            foreach (string s in lines)
+                Console.WriteLine(s);
+        }
+        public void ViewAttendance()
+        {
+            string[] lines = File.ReadAllLines("Attendance.txt");
+            foreach (string s in lines)
+                Console.WriteLine(s);
+        }
+        public void ViewHomework()
+        {
+            string[] lines = File.ReadAllLines("Homework.txt");
+            foreach (string s in lines)
+                Console.WriteLine(s);
+        }
+    }
+}
