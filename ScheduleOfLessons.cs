534fc6e95551edb9783fc6df88e1beb8e6ab665e
levnast <levnast04@gmail.com>
2023-12-05 18:11:54 +0400

    Добавление формы расписания уроков

---
+++ ScheduleOfLessons.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem
+{
+    public class ScheduleOfLessons
+    {
+        public string Object;
+        public TimeOnly Time;
+        public string Classroom;
+        public DateOnly DayOfWeek;
+    }
+}
